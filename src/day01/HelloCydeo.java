bc382d685d4ea3c34fcb176034613c8f39665410
Shemsiye <xemsiye05@gmail.com>
2025-04-10 15:56:41 -0400

    Update HelloCydeo.java

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -3,6 +3,8 @@ package day01;
 public class HelloCydeo {
     public static void main(String[] args) {
         System.out.println("Hello cydeo");
+        System.out.println("Hello world");
+        System.out.println("Hello Shemsiye");
     }
 }
 //green color mean file added to the staging area but not yet to local repository
