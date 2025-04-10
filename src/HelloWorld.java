876aa4df58929dd07d1308ca8b9788639be10c95
shemsiye <xemsiye05@gmail.com>
2025-04-10 00:36:13 -0400

    HelloCydeo Class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,7 @@
 public class HelloWorld {
     //red color mean file has been saved on working directory
+    public static void main(String[] args) {
+        System.out.println("Hello world");
 
+    }
 }
