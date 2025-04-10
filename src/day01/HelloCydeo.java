bb63d6a045e539ecee0a67a17aeb8cd70f32a163
shemsiye <xemsiye05@gmail.com>
2025-04-10 15:40:39 -0400

    Hello cydeo updated

--- src/day01/HelloCydeo.java
+++ src/day01/HelloCydeo.java
@@ -1,6 +1,9 @@
 package day01;
 
 public class HelloCydeo {
+    public static void main(String[] args) {
+        System.out.println("Hello cydeo");
+    }
 }
 //green color mean file added to the staging area but not yet to local repository
 //white color - saved to local
