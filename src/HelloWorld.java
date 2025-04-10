b95abbec7a1a8702ec91c7283aa9692332a0880e
Shemsiye <xemsiye05@gmail.com>
2025-04-10 15:50:58 -0400

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,8 @@ public class HelloWorld {
     //red color mean file has been saved on working directory
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello cydeo");
+        System.out.println("Hello programmers");
 
     }
 }
